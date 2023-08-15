7f74201fe547eb86e800c70063d2ead4fb3b4e47
Jinghui Li <40646108+liangend@users.noreply.github.com>
2023-08-15 11:06:26 -0500

    Add files via upload

--- src/findAligningBases.py
+++ src/findAligningBases.py
@@ -1,11 +1,11 @@
 '''
-This script finds mouse genomic bases that align (at the sequence level) to human genomic on a given human chromosome
+This script finds pig genomic bases that align (at the sequence level) to human genomic on a given human chromosome
 based on axtNet alignment data. Note the following:
 - axtNet alignment data coordinates start from 1, and here we convert it to the 0-based coordinate system used in bed
 files.
-- We process alignments between a positive strand in human and a negative strand in mouse differently than alignments
-between positive strands because the coordinates provided for negative strands in mouse start from the end of the
-chromosome. This requires knowing the size of all mouse chromosomes. More information is found here:
+- We process alignments between a positive strand in human and a negative strand in pig differently than alignments
+between positive strands because the coordinates provided for negative strands in pig start from the end of the
+chromosome. This requires knowing the size of all pig chromosomes. More information is found here:
 https://genome.ucsc.edu/goldenPath/help/axt.html
 '''
 
@@ -22,15 +22,15 @@ def readChromSizes(fn):
     return d
 
 def main():
-    description = 'For a given human chromosome, find all mouse bases that align to that human chromosome'
-    epilog = '# Example for human chromosome 21: python source/findAligningBases.py \
+    description = 'For a given human chromosome, find all pig bases that align to that human chromosome'
+    epilog = '# Example for human chromosome 21: python src/findAligningBases.py \
     -a position/axtNet/chr21.hg19.mm10.net.axt.gz \
     -m position/mm10.chrom.sizes \
     -o position/aligning_bases_by_chrom/hg19.chr21.mm10.basepair.gz'
-    parser = argparse.ArgumentParser(prog='python source/findAligningBases.py', description=description, epilog=epilog)
+    parser = argparse.ArgumentParser(prog='python src/findAligningBases.py', description=description, epilog=epilog)
     g1 = parser.add_argument_group('required arguments')
     g1.add_argument('-a','--axtnet-filename', help='path to human-chromosome-specific axtNet filename', required=True, type=str)
-    g1.add_argument('-m','--mouse-chrom-size-filename', help='path to mm10.chrom.sizes', required=True, type=str)
+    g1.add_argument('-m','--pig-chrom-size-filename', help='path to mm10.chrom.sizes', required=True, type=str)
     g1.add_argument('-o','--output-filename', help='path to human-chromosome-specific output filename', required=True, type=str)
     args = parser.parse_args()
 
@@ -39,7 +39,7 @@ def main():
         args.axtnet_filename = args.axtnet_filename.replace('23','X')
         args.output_filename = args.output_filename.replace('23','X')
     args.output_filename += '.gz' if not args.output_filename.endswith('.gz') else '' # make sure output is .gz
-    mm_chromosome_size = readChromSizes(args.mouse_chrom_size_filename)
+    mm_chromosome_size = readChromSizes(args.pig_chrom_size_filename)
 
     with gzip.open(args.axtnet_filename) as fin, gzip.open(args.output_filename,'wb') as fout:
 
@@ -60,7 +60,7 @@ def main():
             hg_chrom,hg_start,hg_end = l[1].decode('utf-8'),int(l[2]),int(l[3])
             mm_chrom,mm_start,mm_end,mm_strand = l[4].decode('utf-8'),int(l[5]),int(l[6]),l[7].decode("utf-8")=='+'
 
-            # Exclude alignments with mouse chrM or chrY
+            # Exclude alignments with pig chrM or chrY
             if len(mm_chrom)>5 or 'M' in mm_chrom or 'Y' in mm_chrom:
                 for i in range(4): # in order to skip this particular chain, need to skip 4 lines
                     line = fin.readline()
@@ -74,7 +74,7 @@ def main():
                 mm_start -= 1
                 mm_end -= 1
 
-            # If the aligning mouse region is on the negative strand, we need to recompute coordinates
+            # If the aligning pig region is on the negative strand, we need to recompute coordinates
             else:
                 mm_chrom_size = mm_chromosome_size[mm_chrom]
                 s = mm_chrom_size-mm_end
@@ -88,7 +88,7 @@ def main():
 
             for i in range(len(hg_seq)):
                 hg_gap = hg_seq[i]=='-' # set to True if there's a gap in human
-                mm_gap = mm_seq[i]=='-' # set to True if there's a gap in mouse
+                mm_gap = mm_seq[i]=='-' # set to True if there's a gap in pig
 
                 # 1. Gaps in both species
                 if hg_gap and mm_gap:
@@ -98,7 +98,7 @@ def main():
                     else:
                         mm_end -= 1
 
-                # 2. Gap in mouse only
+                # 2. Gap in pig only
                 elif not hg_gap and mm_gap:
                     hg_start += 1
 
